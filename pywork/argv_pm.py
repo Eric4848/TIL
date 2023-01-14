00ac6feaf3927c8e87cbb14c2b67fa223bfbff1e
DESKTOP-6N7LSDG\82108 <hajungsu88@gmail.com>
2023-01-14 21:13:35 +0900

    python 복습 및 파일 업로드

---
+++ pywork/argv_pm.py
@@ -0,0 +1,9 @@
+import sys
+
+print("a")
+
+print(sys.argv)
+
+print(sys.argv[2])
+
+print("b")
---
+++ pywork/imptest.py
@@ -0,0 +1,11 @@
+Pi = 3.141592
+
+class Math:
+    def solv(self, r):
+        return Pi*(r**2)
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("imptest module.")
