74517f1e912bad72e599f8bb7128a5a29b293aae
Eric4848 <hajungsu88@gmail.com>
2023-03-16 21:25:26 +0900

    TIL230316

--- algorithm/programmers/카카오/불량 사용자.py
+++ algorithm/programmers/카카오/불량 사용자.py
@@ -1,12 +1,17 @@
-from itertools import permutations
-
-
 def solution(user_id, banned_id):
-    answer = 0
-    ban = len(banned_id)
+    is_banned = [False] * len(user_id)
     bans = {}
     temp = []
+    banned_users = []
+    counts = {}
     for banned in banned_id:
+        banned_users.append(banned)
+        if banned not in counts:
+            counts[banned] = 1
+
+        elif banned in banned_users:
+            counts[banned] += 1
+
         for user in user_id:
             if len(banned) != len(user):
                 continue
@@ -20,10 +25,30 @@ def solution(user_id, banned_id):
                     break
         bans[banned] = temp
         temp = []
-    print(bans)
-    return answer
+    done = []
+
+    def search(n, check):
+        nonlocal done
+        if n == len(banned_users):
+            check.sort()
+            if check not in done:
+                done.append(check[:])
+            return
+
+        for idx in range(len(user_id)):
+            if is_banned[idx]:
+                continue
+            if user_id[idx] in bans[banned_users[n]]:
+                is_banned[idx] = True
+                check.append(idx)
+                search(n+1, check)
+                check.remove(idx)
+                is_banned[idx] = False
+    search(0, [])
+    return len(done)
 
 
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]))
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
+#print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]))
+#print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
 print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"]))
+#print(solution( ["frido", "frodo"], ["fr*do", "fr**o"]))
