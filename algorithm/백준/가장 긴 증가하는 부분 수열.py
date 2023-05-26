c9be448750104b611101132cf1b72797c130295e
Eric4848 <hajungsu88@gmail.com>
2023-05-26 18:15:45 +0900

    TIL230526

--- algorithm/백준/가장 긴 증가하는 부분 수열.py
+++ algorithm/백준/가장 긴 증가하는 부분 수열.py
@@ -1,28 +1,42 @@
-# dfs - 시간초과
 N = int(input())
+
 arrays = list(map(int, input().split()))
-is_used = [False] * N
 
-maximum = [-float('INF')]
-ans = [0]
+dp = [1 for i in range(N)]
 
+for i in range(N):
+    for j in range(i):
+        if arrays[i] > arrays[j]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-def dfs(s, cnt):
-    if max(ans) > cnt + N - s:
-        return
-
-    if s == N:
-        ans.append(cnt)
-        return
-
-    for i in range(s, N):
-        if arrays[i] > maximum[-1] and not is_used[i]:
-            is_used[i] = True
-            maximum.append(arrays[i])
-            dfs(i+1, cnt+1)
-            is_used[i] = False
-            maximum.pop()
+print(max(dp))
 
 
-dfs(0, 0)
-print(max(ans))
+# # dfs - 시간초과
+# N = int(input())
+# arrays = list(map(int, input().split()))
+# is_used = [False] * N
+#
+# maximum = [-float('INF')]
+# ans = [0]
+#
+#
+# def dfs(s, cnt):
+#     if max(ans) > cnt + N - s:
+#         return
+#
+#     if s == N:
+#         ans.append(cnt)
+#         return
+#
+#     for i in range(s, N):
+#         if arrays[i] > maximum[-1] and not is_used[i]:
+#             is_used[i] = True
+#             maximum.append(arrays[i])
+#             dfs(i+1, cnt+1)
+#             is_used[i] = False
+#             maximum.pop()
+#
+#
+# dfs(0, 0)
+# print(max(ans))
