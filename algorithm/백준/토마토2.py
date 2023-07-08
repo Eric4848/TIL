534386ad9d225d882b9d1a891d5a4c8d954b46d7
Eric4848 <hajungsu88@gmail.com>
2023-07-08 21:42:09 +0900

    TIL230708

--- algorithm/백준/토마토2.py
+++ algorithm/백준/토마토2.py
@@ -6,37 +6,36 @@ M, N, H = map(int, input().split())
 matrix = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 answer = -1
 
-dz = [0, 0, 0, 0, 1, -1]
-dy = [1, -1, 0, 0, 0, 0]
-dx = [0, 0, 1, -1, 0, 0]
+dz = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, 1, -1, 0, 0]
+dx = [0, 0, 0, 0, 1, -1]
 
 queue = deque()
-for i in range(M):
+for i in range(H):
     for j in range(N):
-        for k in range(H):
+        for k in range(M):
             if matrix[i][j][k] == 1:
                 queue.append((i, j, k))
 
 while queue:
-    if 0 not in matrix:
-        print(0)
     answer += 1
     for _ in range(len(queue)):
-        y, x, z = queue.popleft()
+        z, y, x= queue.popleft()
         for i in range(6):
             nz = z + dz[i]
             ny = y + dy[i]
             nx = x + dx[i]
 
-            if -1 < ny < M and -1 < nx < N and -1 < nz < H:
-                if matrix[ny][nx][nz] == 0:
-                    matrix[ny][nx][nz] = 1
-                    queue.append((ny, nx, nz))
+            if -1 < nz < H and -1 < ny < M and -1 < nx < N:
+                if matrix[nz][ny][nx] == 0:
+                    matrix[nz][ny][nx] = 1
+                    queue.append((nz, ny, nx))
 
 check = 0
-for mat in matrix:
-    if 0 in mat:
-        check += 1
+for matz in matrix:
+    for maty in matz:
+        if 0 in maty:
+            check += 1
 
 if check:
     print(-1)
