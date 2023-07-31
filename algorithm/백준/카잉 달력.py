9b1f1bdc1225188c3916a439ef37ae2c7438b6af
Eric4848 <hajungsu88@gmail.com>
2023-07-31 23:55:26 +0900

    TIL230731

--- algorithm/백준/카잉 달력.py
+++ algorithm/백준/카잉 달력.py
@@ -1,26 +1,22 @@
 import sys
-import math
 input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     M, N, x, y = map(int, input().split())
-    cnt = 0
-    lcm = math.lcm(M, N)
-    is_done = False
-    while x < lcm and y < lcm:
-        if x == y:
-            is_done = True
-            break
-        if x < y:
-            x += M
-        else:
-            y += N
+    is_done = False   # 올바른 달력인지 확인
+    # <M-1:N-1> 부터 시작하는 경우에 K = M * a + x = N * b + y를 만족하는 K가 있다고 가정한다
+    K = x   # (K-x) % M = 0 , (K-y) % N = 0인 경우를 계산하기 위해 K = x로 초기화한다
+
+    while K <= M * N:   # K의 값은 M * N을 넘을수 없기 때문에
+        if (K - x) % M == 0 and (K - y) % N == 0:   # K의 조건을 만족하면
+            is_done = True   # 올바름 처리를 하고
+            break   # 멈춘다
+        K += M   # K를 M으로 나눈 나머지이므로 K에 M을 더해나간다
 
     if is_done:
-        print('done')
+        print(K)
     else:
         print(-1)
-
 
 
 # 시간초과
