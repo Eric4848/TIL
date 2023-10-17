1b638895f99641e4cc16f2a349db8e8bf03b1d34
Eric4848 <hajungsu88@gmail.com>
2023-10-17 23:36:26 +0900

    TIL231017

---
+++ Obsidian/Unreal/Programming/C++/c++.md
@@ -0,0 +1,16 @@
+- 헤더파일 = 클래스 선언, 변수와 함수의 원형 
+- 소스파일 = 함수의 내용 저
+## 1. 클래스 생성
+- Tools > New C++ class
+- 클래스 생성 시 상속받을 클래스 선택, 다양한 부모 클래스들이 있다
+- Content Browser에 C++ Classes에서 확인할 수 있다
+- 클래스 생성 시 헤더파일(.h)과 소스파일(.cpp)가 생성된다
+## 2. 함수
+1. 클래스 이름의 함수 - 객체가 생성될 때 실행, 주로 액터의 기본값을 설정
+2. BeginPlay - 배치된 월드에서 게임 시작, 스폰되었을 때 호출, 게임 플레이 로직 초기화
+3. Tick - 프레임마다 호출, 매개변수 DeltaTime을 이용해 함수 호출간 시간 계산, 주로 게임의 로직 계산, 함수 제거로 성능 향상 가능
+	*함수 제거를 위해 함수들을 제거하고 소스파일에서 "PrimaryActorTick.bCanEverTick = True(매초마다 Tick함수 호출하는 코드);"를 제거한다*
+## 3. 컴파일
+- cmd에서 LiveCoding.Compile 입력
+- Ctrl + Alt + F11
+- Unreal Editor 종료 > VS 상단 Build > Rebuild Solution > 다시 시
--- algorithm/백준/이중 우선순위 큐.py
+++ algorithm/백준/이중 우선순위 큐.py
@@ -1,43 +1,119 @@
-# 시간 초과
+import sys
 import heapq
 
-
-def rev(Q):
-    temp = []
-    for _ in range(len(Q)):
-        heapq.heappush(temp, -heapq.heappop(Q))
-    return temp
-
-
-T = int(input())
-for _ in range(T):
-    flag = 0
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
     k = int(input())
-    q = []
-    for _ in range(k):
-        order, num = input().split()
-        if order == 'I':
-            heapq.heappush(q, int(num))
-
+    minQ, maxQ = [], []   # 최대, 최소를 저장할 힙
+    deleted = [True] * k   # 삭제된 입력들을 확인할 리스트
+    for i in range(k):
+        com, n = input().split()
+        n = int(n)
+        if com == 'I':
+            heapq.heappush(minQ, (n, i))   # 최소
+            heapq.heappush(maxQ, (-n, i))   # 최대 힙에 각각 몇번째로 넣었는지와 함꼐 추가
+            deleted[i] = False   # 삭제된 입력 리스트 변경
         else:
-            if q:
-                if int(num) == -1:
-                    if flag:
-                        q = rev(q)
-                        heapq.heappop(q)
-                        flag = 0
-                    else:
-                        heapq.heappop(q)
-                else:
-                    if flag:
-                        heapq.heappop(q)
-                    else:
-                        q = rev(q)
-                        heapq.heappop(q)
-                        flag = 1
-    if not q:
-        print('EMPTY')
+            if n == 1:   # 최댓값을 빼는 경우
+                while maxQ and deleted[maxQ[0][1]]:   # 최댓값 힙에 숫자별로, 해당 숫자가 삭제된 숫자면
+                    heapq.heappop(maxQ)   # 해당 숫자를 pop해준다
+                if maxQ:   # 그 후 최댓값 힙이 비지 않았으면
+                    deleted[maxQ[0][1]] = True   # 삭제함 처리를 하고
+                    heapq.heappop(maxQ)   # 최댓값을 제거한다
+            else:   # 최솟값을 빼는 경우
+                while minQ and deleted[minQ[0][1]]:   # 최댓값과 동일하다
+                    heapq.heappop(minQ)
+                if minQ:
+                    deleted[minQ[0][1]] = True
+                    heapq.heappop(minQ)
+
+    # 연산이 끝난 후 삭제된 값들 제거
+    while minQ and deleted[minQ[0][1]]:
+        heapq.heappop(minQ)
+    while maxQ and deleted[maxQ[0][1]]:
+        heapq.heappop(maxQ)
+
+    if minQ and maxQ:
+        print(-maxQ[0][0], minQ[0][0])
     else:
-        if flag:
-            q = rev(q)
-        print(q[0], q[-1])
+        print('EMPTY')
+
+# 시간 초과
+# import heapq
+# import sys
+#
+# input = sys.stdin.readline
+#
+#
+# def rev(Q):
+#     temp = []
+#     for _ in range(len(Q)):
+#         heapq.heappush(temp, -heapq.heappop(Q))
+#     return temp
+#
+#
+# T = int(input())
+# for _ in range(T):
+#     flag = 0
+#     k = int(input())
+#     q = []
+#     for _ in range(k):
+#         order, num = input().split()
+#         if order == 'I':
+#             if not flag:
+#                 heapq.heappush(q, int(num))
+#             else:
+#                 heapq.heappush(q, -int(num))
+#
+#         else:
+#             if q:
+#                 if int(num) == -1:
+#                     if flag:
+#                         q = rev(q)
+#                         heapq.heappop(q)
+#                         flag = 0
+#                     else:
+#                         heapq.heappop(q)
+#                 else:
+#                     if flag:
+#                         heapq.heappop(q)
+#                     else:
+#                         q = rev(q)
+#                         heapq.heappop(q)
+#                         flag = 1
+#     if not q:
+#         print('EMPTY')
+#     else:
+#         if flag:
+#             q = rev(q)
+#         print(q[-1], q[0])
+
+
+# 오답
+# import heapq
+# import sys
+#
+# input = sys.stdin.readline
+# T = int(input())
+# for _ in range(T):
+#     k = int(input())
+#     q = []
+#     for _ in range(k):
+#         print(q)
+#         order, num = input().split()
+#         if order == 'I':
+#             heapq.heappush(q, int(num))
+#
+#         else:
+#             if q:
+#                 if int(num) == -1:
+#                     heapq.heappop(q)
+#                 else:
+#                     q.pop()
+#
+#     if not q:
+#         print('EMPTY')
+#     else:
+#         print(q[-1], q[0])
+
