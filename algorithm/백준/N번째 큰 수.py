0e8dba79021d27930ee00e355b86abcd4db0c822
Eric4848 <hajungsu88@gmail.com>
2023-12-06 23:48:01 +0900

    TIL231206

--- algorithm/백준/N번째 큰 수.py
+++ algorithm/백준/N번째 큰 수.py
@@ -1,15 +1,32 @@
-# 메모리 초과
-import sys
+import heapq
 
-input = sys.stdin.readline
+heap = []   # 힙 초기화
 N = int(input())
-over_nums = []
-for i in range(N):
-    nums = list(map(int, input().split()))
-    nums += over_nums
-    nums.sort()
-    over_nums = nums[N-1-i:]
-print(nums[-N])
+
+for _ in range(N):
+    nums = map(int, input().split())
+    for num in nums:   # 각 줄별로
+        if len(heap) < N:   # 힙 크기가 N보다 작다면
+            heapq.heappush(heap, num)   # 힙에 추가한다
+        else:   # 힙 크기가 N개 이상이면
+            if heap[0] < num:   # 힙의 가장 작은 수보다 현재 숫자가 크면
+                heapq.heappop(heap)   # 가장 작은 수를 제거하고
+                heapq.heappush(heap, num)   # 현재 숫자를 힙에 추가한다
+print(heap[0])
+
+
+# 메모리 초과
+# import sys
+#
+# input = sys.stdin.readline
+# N = int(input())
+# over_nums = []
+# for i in range(N):
+#     nums = list(map(int, input().split()))
+#     nums += over_nums
+#     nums.sort()
+#     over_nums = nums[N-1-i:]
+# print(nums[-N])
 
 
 # 메모리 초과
