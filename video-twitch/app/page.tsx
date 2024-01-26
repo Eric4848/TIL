a2c1b708f78f713a27d803bfa050b12c55c7c9a4
Eric4848 <hajungsu88@gmail.com>
2024-01-26 23:12:02 +0900

    TIL240126

--- video-twitch/app/page.tsx
+++ video-twitch/app/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col gap-y-4'>
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl='/' />
       <p className='text-red-500 font-bold'>Hello Twitch Clone</p>
-      <Button size="lg" variant="outline">
+      <Button
+        size='lg'
+        variant='outline'
+        style={{ width: 200 }}
+      >
         Click me!
       </Button>
     </div>
