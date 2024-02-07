9b91bff7d98cd53c0b14a276ce0fc2e16373987b
Eric4848 <hajungsu88@gmail.com>
2024-02-07 22:35:10 +0900

    TIL240207

---
+++ algorithm/백준/이차원 배열과 연산.py
@@ -0,0 +1,71 @@
+r, c, k = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(3)]
+chk = 1
+
+
+def calcrow():
+    mat = []
+    maxlen = 0
+    length = len(matrix)
+    for i in range(len(matrix)):
+        cnt = [0] * (length + 1)
+        for j in range(len(matrix[i])):
+            cnt[matrix[i][j]] += 1
+        tmp = []
+        for a in range(1, (length + 1)):
+            for b in range(1, (length + 1)):
+                if cnt[b] == a:
+                    tmp.append(b)
+                    tmp.append(a)
+        maxlen = max(maxlen, len(tmp))
+        mat.append(tmp)
+    for i in range(len(mat)):
+        for _ in range(maxlen-len(mat[i])):
+            mat[i].append(0)
+
+    return mat
+
+
+def calccol():
+    mat = []
+    maxlen = 0
+    length = len(matrix[0])
+    for i in range(len(matrix[0])):
+        cnt = [0] * (length + 1)
+        for j in range(len(matrix)):
+            cnt[matrix[j][i]] += 1
+        tmp = []
+        for a in range(1, (length + 1)):
+            for b in range(1, (length + 1)):
+                if cnt[b] == a:
+                    tmp.append(b)
+                    tmp.append(a)
+        maxlen = max(maxlen, len(tmp))
+        mat.append(tmp)
+
+    for i in range(len(mat)):
+        for _ in range(maxlen-len(mat[i])):
+            mat[i].append(0)
+
+    rotmat = [[] for _ in range(len(mat[0]))]
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            rotmat[j].append(mat[i][j])
+    return rotmat
+
+
+for i in range(101):
+    if r-1 < len(matrix) and c-1 < len(matrix[0]):
+        if matrix[r-1][c-1] == k:
+            print(i)
+            chk = 0
+            break
+
+    if len(matrix[0]) <= len(matrix):
+        matrix = calcrow()
+
+    else:
+        matrix = calccol()
+
+if chk:
+    print(-1)
